d60c6aebedfb0b7b299e50f5365a171f5e9bfe38
Haris <harismuha123@gmail.com>
2018-04-06 19:19:55 +0200

    changed login layout, changed color scheme, fixed minor bugs

--- app/src/main/java/ba/edu/ibu/stu/chern0/payscan/MainActivity.java
+++ app/src/main/java/ba/edu/ibu/stu/chern0/payscan/MainActivity.java
@@ -1,28 +1,78 @@
 package ba.edu.ibu.stu.chern0.payscan;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnFocusChangeListener{
+
+    private EditText email;
+    private EditText password;
+    private TextView emailTitle;
+    private TextView passwordTitle;
+    private TextView createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         //Installing Calligraphy font library, adding default font path leading to Raleway-Light.ttf
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/Raleway-Light.ttf")
                 .setFontAttrId(R.attr.fontPath)
+                .addCustomStyle(AppCompatButton.class, android.R.attr.buttonStyle)
                 .build()
         );
+        setContentView(R.layout.activity_main);
+        email = findViewById(R.id.textEmail);
+        password = findViewById(R.id.textPassword);
+        emailTitle = findViewById(R.id.emailTitle);
+        passwordTitle = findViewById(R.id.passwordTitle);
+        createAccount = findViewById(R.id.account);
+
+//        createAccount.setPaintFlags(createAccount.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        email.setOnFocusChangeListener(this);
+        password.setOnFocusChangeListener(this);
+
+
     }
 
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+
+        if (email.hasFocus()) {
+            emailTitle.animate().alpha(1).setDuration(250);
+            email.setHint("");
+        } else {
+            emailTitle.animate().alpha(0).setDuration(250);
+            email.setHint("E-mail");
+        }
+
+        if (password.hasFocus()) {
+            passwordTitle.animate().alpha(1).setDuration(250);
+            password.setHint("");
+        } else {
+            passwordTitle.animate().alpha(0).setDuration(250);
+            password.setHint("Password");
+        }
     }
 }
