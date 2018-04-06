bb187b2315ef49b2f490e6c007f2ff957d5994ae
Haris <harismuha123@gmail.com>
2018-04-06 17:28:10 +0200

    created project, created Login page, added custom fonts to app

--- README.md
+++
@@ -1,5 +0,0 @@
-# payscan-mobile-app-android
-Simple payment app for OLX users and similar.
-
-
-https://www.ibu.edu.ba/
---
+++ app/src/main/java/ba/edu/ibu/stu/chern0/payscan/MainActivity.java
@@ -0,0 +1,28 @@
+package ba.edu.ibu.stu.chern0.payscan;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Installing Calligraphy font library, adding default font path leading to Raleway-Light.ttf
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Raleway-Light.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
