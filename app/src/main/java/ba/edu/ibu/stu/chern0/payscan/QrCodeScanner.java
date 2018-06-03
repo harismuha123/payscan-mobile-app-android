531a0323238dfb70fc77893be8a203618db332c3
Aldin-SXR <aldin.kovacevic.97@gmail.com>
2018-06-03 18:19:54 +0200

    Added QR code generator and scanner

--- app/src/main/java/ba/edu/ibu/stu/chern0/payscan/QrCodeScanner.java
+++ app/src/main/java/ba/edu/ibu/stu/chern0/payscan/QrCodeScanner.java
@@ -8,6 +8,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 
@@ -46,6 +47,7 @@ public class QrCodeScanner extends AppCompatActivity implements ZXingScannerView
         Log.e("format", rawResult.getBarcodeFormat().toString());
         Intent intent = new Intent();
         intent.putExtra("text", rawResult.getText());
+        Toast.makeText(this, rawResult.getText(), Toast.LENGTH_LONG).show();
         setResult(RESULT_OK, intent);
         finish();
     }
